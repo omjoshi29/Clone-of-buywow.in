859bdad5cfbf77d88892a2a921bef325e3909d19
123bhagesh <123bhagesh@gmail.com>
2022-03-31 23:15:11 +0530

    Adding Navbar, footer and addToCart funcanility is Done

--- Nutration_&_health_bhagesh/nutratation_script/immunity_care.js
+++ Nutration_&_health_bhagesh/nutratation_script/immunity_care.js
@@ -50,6 +50,7 @@ function appendProduct(wowProducts) {
       localStorage.setItem("cartArr", JSON.stringify(cartArr));
       console.log(cartArr);
       alert("Product added to the cart!");
+      window.location.href="./../cart.html"
     })
 
     let box = document.createElement("div");
